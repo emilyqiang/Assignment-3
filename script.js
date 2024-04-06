d5b8f9cf2659ee23d3c5ee20e48f2cf3162b76ec
emilyqiang <158528986+emilyqiang@users.noreply.github.com>
2024-04-06 11:54:04 -0700

    grids, video and swiper pain

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 //linking object to mySwiper element
 var swiper = new Swiper(".mySwiper", {
+    //slide settings
+    slidesPerView: "auto",
+    spaceBetween: 30,
     //linking functions to elements
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
+
+    pagination: {
+      el: '.swiper-pagination',
+      dynamicBullets: true,
+    }
   });
 
