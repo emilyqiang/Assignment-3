a79732620ec570968f2d5494dbab9ea662962e4b
emilyqiang <158528986+emilyqiang@users.noreply.github.com>
2024-04-06 13:18:06 -0700

    finished works page yay

--- script.js
+++ script.js
@@ -1,17 +1,65 @@
 //linking object to mySwiper element
 var swiper = new Swiper(".mySwiper", {
-    //slide settings
-    slidesPerView: "auto",
-    spaceBetween: 30,
-    //linking functions to elements
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
+  //slide settings
+  slidesPerView: "auto",
+  spaceBetween: 30,
+  //linking functions to elements
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 
-    pagination: {
-      el: '.swiper-pagination',
-      dynamicBullets: true,
-    }
-  });
+  pagination: {
+    el: '.swiper-pagination',
+    dynamicBullets: true,
+  }
+});
+
+var swiper = new Swiper(".mySwiper2", {
+  //slide settings
+  slidesPerView: "auto",
+  spaceBetween: 30,
+  //linking functions to elements
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  pagination: {
+    el: '.swiper-pagination',
+    dynamicBullets: true,
+  }
+});
+
+var swiper = new Swiper(".mySwiper3", {
+  //slide settings
+  slidesPerView: "auto",
+  spaceBetween: 30,
+  //linking functions to elements
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  pagination: {
+    el: '.swiper-pagination',
+    dynamicBullets: true,
+  }
+});
+
+var swiper = new Swiper(".mySwiper4", {
+  //slide settings
+  slidesPerView: "auto",
+  spaceBetween: 30,
+  //linking functions to elements
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  pagination: {
+    el: '.swiper-pagination',
+    dynamicBullets: true,
+  }
+});
 
