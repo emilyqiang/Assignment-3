dccce14379795d3a21165b2a23132b016e104283
emilyqiang <158528986+emilyqiang@users.noreply.github.com>
2024-04-06 00:02:10 -0700

    more responsiveness and fixed grids etc cryihng

--- script.js
+++ script.js
@@ -1,21 +1,9 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'vertical',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
+//linking object to mySwiper element
+var swiper = new Swiper(".mySwiper", {
+    //linking functions to elements
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
     },
   });
+
